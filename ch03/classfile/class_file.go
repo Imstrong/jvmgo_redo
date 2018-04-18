adcfe79764f1d8e0ed90905e309d4a8b0941faff
装逼之王 <zbzwcode@163.com>
2018-04-18 17:30:50 +0800

    add data types

--- ch03/classfile/class_file.go
+++ ch03/classfile/class_file.go
@@ -42,8 +42,25 @@ func (self *ClassFile) read(reader *ClassReader) {
 	self.methods=readMembers(reader,self.constantPool)
 	self.attributes=readAttributes(reader,self.constantPool)
 }
-func (self *ClassFile) readAndCheckMagic(reader *ClassReader) {}
-func (self *ClassFile) readAndCheckVersion(reader *ClassReader) {}
+func (self *ClassFile) readAndCheckMagic(reader *ClassReader) {
+	magic:=reader.readUint32()
+	if magic != 0xCAFEBABE {
+		panic("java.lang.ClassFormatError: magic!")
+	}
+}
+func (self *ClassFile) readAndCheckVersion(reader *ClassReader) {
+	self.minorVersion=reader.readUint16()
+	self.majorVersion=reader.readUint16()
+	switch self.majorVersion {
+	case 45:
+		return
+	case 46,47,48,49,50,51,52:
+		if self.minorVersion==0 {
+			return
+		}
+	}
+	panic("java.lang.UnsupportedClassVersionError!")
+}
 func (self *ClassFile) MinorVersion() uint16 {
 	return self.minorVersion
 } // getter
