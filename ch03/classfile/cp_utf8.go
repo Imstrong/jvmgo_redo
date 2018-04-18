317d966ceb099af9ef8ed4565050542af1780465
bruce_vae <zbzwcode@163.com>
2018-04-19 00:57:49 +0800

    nothing

--- ch03/classfile/cp_utf8.go
+++ ch03/classfile/cp_utf8.go
@@ -7,3 +7,6 @@ func (self *ConstantUtf8Info) readInfo(reader *ClassReader){
 	bytes:=reader.readBytes(length)
 	self.str=decodeMUTF8(bytes)
 }
+func decodeMUTF8(bytes []byte)string {
+	return string(bytes)
+}
